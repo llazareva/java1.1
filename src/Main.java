bc699455bebdc5955516ab17a567f83b88b14107
Lina <llazareva7708@gmail.com>
2021-09-22 13:29:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000000_000;
+        int transfer = 500_000_000;
+       int totalBalance = balance + transfer;
+        System.out.println(totalBalance);
+    }
+}
